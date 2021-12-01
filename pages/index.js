5a82212c3fb47a7fc96b57c0ae020798d487e062
Son Beomsoo <k1a11220@naver.com>
2021-12-01 17:20:57 +0900

    Connect with graphql api

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,48 @@
 import styled from "styled-components";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 const Container = styled.div`
   height: 120vh;
   background-color: var(--bgColor);
 `;
 
-const Home = () => {
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "http://localhost:4000/",
+    cache: new InMemoryCache(),
+  });
+  const { data } = await client.query({
+    query: gql`
+      {
+        benefits {
+          id
+          name
+          category
+          location
+          address
+          summary
+          thumbnail
+          mainBenefit
+          benefits
+          conditions
+          othersOpts
+          dtlImgs
+          dtlLink
+          contactNb
+          period
+        }
+      }
+    `,
+  });
+  return {
+    props: {
+      benefits: data.benefits,
+    },
+  };
+}
+
+const Home = ({ benefits }) => {
+  console.log("benefits", benefits);
   return <Container>This is Home</Container>;
 };
 
